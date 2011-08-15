2363a35b53c3cd6c40f1a210fa98d9868cea19dc
Robert Böhm <robert.boehm94@gmail.com>
2011-08-15 21:44:47 +0200

    Added license information.

--- OpenLima/OpenLima/src/openlima/graphics/Window.hpp
+++ OpenLima/OpenLima/src/openlima/graphics/Window.hpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2011 Robert Böhm
+// This file is part of OpenLima.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenLima. If not, see: <http://www.gnu.org/licenses/>.
+
 #ifndef OPENLIMA_GRAPHICS_WINDOW_HPP
 #define OPENLIMA_GRAPHICS_WINDOW_HPP
 
--- OpenLima/OpenLima/src/openlima/input/GlobalMouse.cpp
+++ OpenLima/OpenLima/src/openlima/input/GlobalMouse.cpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2011 Robert Böhm
+// This file is part of OpenLima.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenLima. If not, see: <http://www.gnu.org/licenses/>.
+
 #include <GL/freeglut_std.h>
 #include "GlobalMouse.hpp"
 
