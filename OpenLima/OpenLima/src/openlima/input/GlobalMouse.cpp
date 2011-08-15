0bff95f06fcf7e064a8585cffd21364dc69c0ad5
Robert Böhm <robert.boehm94@gmail.com>
2011-08-16 00:00:12 +0200

    Changed every "ö" to "oe", due to some charset problems.

--- OpenLima/OpenLima/src/openlima/input/GlobalMouse.cpp
+++ OpenLima/OpenLima/src/openlima/input/GlobalMouse.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2011 Robert Böhm
+// Copyright (C) 2011 Robert Boehm
 // This file is part of OpenLima.
 // 
 // You should have received a copy of the GNU Lesser General Public License
